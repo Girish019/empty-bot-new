546889e670834a350a9466a60e2a703a499398a1
Kolminch <122073398+Kolminch@users.noreply.github.com>
2024-08-09 20:41:06 +0530

    Update automate_genlink.py

--- plugins/automate_genlink.py
+++ plugins/automate_genlink.py
@@ -1,8 +1,111 @@
 
+from pyrogram import filters, Client
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+
+
+from plugins.cbb import DATEDAY
+# from bot import Bot
+# from config import ADMINS, CHANNEL_ID, DISABLE_CHANNEL_BUTTON
+# from helper_func import encode
+# import re
+
+import aiohttp
+import asyncio
+from pyrogram import filters, Client, enums
+from pyrogram.errors import FloodWait
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from pyrogram.errors.exceptions.bad_request_400 import ChannelInvalid, UsernameInvalid, UsernameNotModified
+from config import ADMINS, LOG_CHANNEL, PUBLIC_FILE_STORE, WEBSITE_URL, WEBSITE_URL_MODE
+from plugins.data import DATAODD, DATAEVEN ,BOTEFITMSG, FOMET
+from plugins.users_api import get_user, get_short_link
+from plugins.database import unpack_new_file_id
+from datetime import datetime
+import re
+import os
+import json
+import requests
+import string
+import random
+import base64
+import logging
+
+
+@Client.on_message(filters.private & filters.user(ADMINS) & filters.command(["date"]))
+async def date(bot, message):
+    dat = await message.reply_text("Select your Date.........",quote=True,reply_markup=InlineKeyboardMarkup([[ 
+        			InlineKeyboardButton("Yesterday",callback_data='ystdy'), 
+        			InlineKeyboardButton("Today",callback_data = 'tdy'), 
+        			InlineKeyboardButton("Tommorow",callback_data='tmr') ]]))
+    
+
+@Client.on_message(filters.private & filters.user(ADMINS) & ~filters.text)
+async def channel_post(client: Client, message: Message):
+    current_time = datetime.now()
+    media = message.video or message.document
+    
+    bot_msg = await message.reply_text("Please Wait...!", quote = True) #reply text please wait... to bot
+    try:
+        #janvary = current_time.strftime("%B")
+        medias = media.file_name.replace(".","_")
+        filname = re.split("S\d\d", medias)[0]#[1][2]etc
+        Eno= re.findall("S\d+E\d+\d", medias)
+        if len(DATEDAY)==0:
+            await client.send_message(chat_id=message.chat.id, text="Error: invalid date please set /date")
+            bot_msg.delete()
+            return 
+        else:                
+            if int(DATEDAY[-1][0:2]) % 2 != 0:#chaeking for ODD by given date
+                if filname in DATAODD.keys(): #matching name in dict key with arrival video file name
+                    await asyncio.sleep(1)
+                    chtid=int(DATAODD[filname][3])#for particular channel id
+                    pic=DATAODD[filname][0] #particuler images
+                    SL_URL=DATAODD[filname][1] #for particuler domine name
+                    SL_API=DATAODD[filname][2] #for particuler api 
+                   # chtid=message.chat.id # if you want pic+formet into bot pm     
+        
+            elif int(DATEDAY[-1][0:2]) % 2 == 0: #checking for EVEN
+                if filname in DATAEVEN.keys():
+                    await asyncio.sleep(1)
+                    chtid=int(DATAEVEN[filname][3])
+                    pic=DATAEVEN[filname][0]
+                    SL_URL=DATAEVEN[filname][1]
+                    SL_API=DATAEVEN[filname][2]
+                    # chtid=message.chat.id # if you want pic+formet into bot pm
+            Size = await get_size(media.file_size)
+            await bot_msg.edit("Getting size....!")
+            await asyncio.sleep(1)
+            Tlink = await conv_link(client , message)
+            await bot_msg.edit("Tlink generating....!")
+            await asyncio.sleep(1)
+            Slink = await get_short(SL_URL, SL_API, Tlink)
+            await bot_msg.edit("Slink generating....!")
+            await asyncio.sleep(1)
+            await bot_msg.edit("Sending post......!")
+            await asyncio.sleep(1)
+            if Slink:
+                await client.send_photo(chat_id=chtid, photo=pic, caption=FOMET.format(DATEDAY[-1], Eno[0], Size, Slink, Slink))
+                await bot_msg.edit(BOTEFITMSG.format(filname, Tlink, Slink, Size, DATEDAY[-1])) # msg edit in forwarder channel = "pic without captions (see line 41)" ==> thats return to our given format and short link ,date are updated here
+                return 
+            else:
+                Slink = "ERORR_ACCURED"
+                await message.reply_photo(photo=pic, caption=FOMET.format(DATEDAY[-1], Eno[0], Size, Slink, Slink), quote = True)
+                await bot_msg.edit(f"<b>Here is your link</b>\n\n<code>{Tlink}</code>\n\n<b>Filename :</b> {medias}")
+                return 
+    except Exception as e:
+        link = await conv_link(client , message)
+        await bot_msg.edit(f"<b>Here is your link</b>\n\n{link}\n\n<code>{link}</code>\n\n<b>Exception couse :</b> {e}\n\n<b>Filename :</b> {medias}")
+        Slink = "ERORR_ACCURED"
+        await message.reply_photo(photo=pic, caption=FOMET.format(DATEDAY[-1], Eno[0], Size, Slink, Slink), quote = True))
 
 
 
-@Bot.on_message((filters.document | filters.video | filters.audio) & filters.private)
+
+
+
+
+
+
+
 async def convert_link(bot, message):
     username = (await bot.get_me()).username
     file_type = message.media
@@ -12,5 +115,5 @@ async def convert_link(bot, message):
     outstr = base64.urlsafe_b64encode(string.encode("ascii")).decode().strip("=")
     user_id = message.from_user.id
     share_link = f"https://t.me/{username}?start={outstr}"
-    await message.reply(f"<b>⭕ ʜᴇʀᴇ ɪs ʏᴏᴜʀ ʟɪɴᴋ:\n\n🔗 ᴏʀɪɢɪɴᴀʟ ʟɪɴᴋ :- {share_link}</b>")
-    return
+    # await message.reply(f"<b>⭕ ʜᴇʀᴇ ɪs ʏᴏᴜʀ ʟɪɴᴋ:\n\n🔗 ᴏʀɪɢɪɴᴀʟ ʟɪɴᴋ :- {share_link}</b>")
+    return share_link
