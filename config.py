18527a7bbe17be4621c368e0d4380f1eb21a8bf7
Girish019 <118633556+Girish019@users.noreply.github.com>
2024-10-19 15:41:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ ADMINS.append(1284476297)
 ADMINS.append(5636224141)
 
 # Clone Info :-
-# CLONE_MODE = bool(environ.get('CLONE_MODE', True)) # Set True or False
+CLONE_MODE = bool(environ.get('CLONE_MODE', True)) # Set True or False
 
 # If Clone Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://Tessa:tessa33@practice1.3mqtowb.mongodb.net/?retryWrites=true&w=majority")
